0c4a826910ae284731186460ec20814a6bb5b41b
bead8785 <166611759+bead8785@users.noreply.github.com>
2024-04-18 16:55:20 +0900

    Update upbit_auto.py

--- upbit_auto.py
+++ upbit_auto.py
@@ -106,9 +106,7 @@ while True:
                     target_price = get_target_price(ticker, 0.5)
                     ma15 = get_ma15(ticker)
                     current_price = get_current_price(ticker)
-                    print(f"{coin['name']} 보유량: {balance}, 평균매수가: {avg_buy_price}, 현재가: {current_price}, 목표가: {target_price}, 15일 이동평균가: {ma15}")
-                    message = f"{coin['name']} 보유량: {balance}, 평균매수가: {avg_buy_price}, 현재가: {current_price}, 목표가: {target_price}, 15일 이동평균가: {ma15}"
-                    send_slack_message(message)
+                    print(f"{coin['name']} 보유량: {balance:,.6f}, 평균매수가: {avg_buy_price:,.6f}, 현재가: {current_price:,.6f}, 목표가: {target_price:,.6f}, 15일 이동평균가: {ma15:,.6f}")
                     if target_price < current_price and ma15 < current_price:
                         krw = get_balance("KRW")
                         if krw > 5000:
@@ -116,16 +114,14 @@ while True:
                             upbit.buy_market_order(ticker, buy_amount)
                             # 매수한 내역 저장
                             buy_history.append({"coin": coin['name'], "amount": buy_amount})
-                            print(f"매수: {coin['name']} {buy_amount} 원")
+                            print(f"매수: {coin['name']} {buy_amount:,.6f} 원")
                 else:
                     if ma15 > current_price:
                         loss_rate = (current_price - avg_buy_price) / avg_buy_price
                         if loss_rate < -0.03:
                             sell_amount = balance * 0.9995
                             upbit.sell_market_order(ticker, sell_amount)
-                            print(f"매도: {coin['name']} 매도가: {current_price}, 손실률: {loss_rate}")
-                            message = f"매도: {coin['name']} 매도가: {current_price}, 손실률: {loss_rate}"
-                            send_slack_message(message)
+                            print(f"매도: {coin['name']} 매도가: {current_price:,.6f}, 손실률: {loss_rate}")
         if len(holding_coins) == 0:
             for coin in coin_list:
                 ticker = f"KRW-{coin['ticker']}"
@@ -135,17 +131,12 @@ while True:
                     target_price = get_target_price(ticker, 0.5)
                     ma15 = get_ma15(ticker)
                     current_price = get_current_price(ticker)
-                    print(f"{coin['name']} 현재가: {current_price}, 목표가: {target_price}, 15일 이동평균가: {ma15}")
-                    message = f"{coin['name']} 현재가: {current_price}, 목표가: {target_price}, 15일 이동평균가: {ma15}"
-                    send_slack_message(message)
+                    print(f"{coin['name']} 현재가: {current_price:,.6f}, 목표가: {target_price:,.6f}, 15일 이동평균가: {ma15:,.6f}")
                     if target_price < current_price and ma15 < current_price:
                         krw = get_balance("KRW")
                         if krw > 5000:
                             upbit.buy_market_order(ticker, krw*0.9995)
-                            print(f"매수: {coin['name']} 매수가: {current_price}, 목표가: {target_price}, 평균가: {avg_buy_price}")
-                            message = f"매수: {coin['name']} 매수가: {current_price}, 목표가: {target_price}, 평균가: {avg_buy_price}"
-                            send_slack_message(message)
-                            
+                            print(f"매수: {coin['name']} 매수가: {current_price:,.6f}, 목표가: {target_price:,.6f}, 평균가: {avg_buy_price:,.6f}")
         time.sleep(1)
     except Exception as e:
         print(e)
